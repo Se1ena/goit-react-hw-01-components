7a6affdb04856d19ce036af856e81af8eea54abd
Veronika Moskaliuk <veronika.moskaliuk.ua@gmail.com>
2022-12-10 18:52:27 +0100

    added styles

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -4,7 +4,7 @@ import css from 'components/FriendListItem/FriendListItem.module.css';
 const FriendListItem = ({avatar, name, isOnline}) => {
 return(
     <li className={css.item}>
-  <span className={isOnline ? css.isOnline : css.status}></span>
+  <span className={`${css.status} ${isOnline ? css.isOnline : css.isOffline}`}></span>
   <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
   <p className={css.name}>{name}</p>
 </li>
